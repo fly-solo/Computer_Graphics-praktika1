98e71724fb39cea2dfe5def998893e0f27967dd2
fly-solo <95430097+fly-solo@users.noreply.github.com>
2022-03-25 19:19:12 +0500

    Add files via upload

--- Praktika inzhenerka 1.cpp
+++ Praktika inzhenerka 1.cpp
@@ -1,11 +1,20 @@
 ﻿
 #include <iostream>
+#include <GL/glew.h>
 #include <Gl/freeglut.h>
 #include <stdio.h>
+#include <glm/vec3.hpp>
+GLuint VBO;
 static void RenderSceneCB()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDisableVertexAttribArray(0);
 	glutSwapBuffers();
+	
 }
 int main(int argc, char *argv[])
 {
@@ -15,8 +24,21 @@ int main(int argc, char *argv[])
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Tutorial 01");
 	glutDisplayFunc(RenderSceneCB);
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+
+	glm::vec3 Vertices[3];
+	Vertices[0] = glm::vec3(-1.0f, 1.0f, 0.0f);
+	Vertices[1] = glm::vec3(-1.0f, -1.0f, 0.0f);
+	Vertices[2] = glm::vec3(1.0f, 0.0f, 0.0f);
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+	glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
 	glutMainLoop();
-	glClear(GL_COLOR_BUFFER_BIT);
-	glutSwapBuffers();
 }
