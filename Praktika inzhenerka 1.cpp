ce51a19afbd93e56e8f987d7c4bf3a625eba7bc8
fly-solo <95430097+fly-solo@users.noreply.github.com>
2022-03-25 17:45:40 +0500

    Add files via upload

---
+++ Praktika inzhenerka 1.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <Gl/freeglut.h>
+#include <stdio.h>
+static void RenderSceneCB()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Tutorial 01");
+	glutDisplayFunc(RenderSceneCB);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glutMainLoop();
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
