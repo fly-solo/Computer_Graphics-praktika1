f1b8df2701766781e13751efda3d8723bec08c58
fly-solo <95430097+fly-solo@users.noreply.github.com>
2022-03-25 18:15:40 +0500

    Add files via upload

--- Praktika inzhenerka 1.cpp
+++ Praktika inzhenerka 1.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Tutorial 01");
 	glutDisplayFunc(RenderSceneCB);
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
 	glutMainLoop();
 	glClear(GL_COLOR_BUFFER_BIT);
 	glutSwapBuffers();
